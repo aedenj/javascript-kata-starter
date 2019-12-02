670f7923da5c4f5098004a164749f2c1f6c550ce
Aeden Jameson <aeden.jameson@gmail.com>
2019-12-02 08:00:48 -0800

    undo accidental overwrites

--- index.spec.js
+++ index.spec.js
@@ -1,23 +1,8 @@
 import { expect } from "chai"
-import { kClosest } from "./index"
 
 
-describe("k closest points", () => {
-  describe("[[3,3],[5,-1],[-2,4]] and k=2", () => {
-    it("returns [[3,3],[-2,4]]", () => {
-      expect(kClosest([[3,3],[5,-1],[-2,4]], 2)).to.deep.equal([[3,3],[-2,4]])
-    })
-  })
-
-  describe("[[0,1],[1,0]] and k=2", () => {
-    it("returns [[1,0]]", () => {
-      expect(kClosest([[0,1],[1,0]], 2)).to.deep.equal([[0,1],[1,0]])
-    })
-  })
-
-  describe("[[6,10],[-3,3],[-2,5],[0,2]] and k=3", () => {
-    it("returns [[0,2],[-3,3],[-2,5]]", () => {
-      expect(kClosest([[6,10],[-3,3],[-2,5],[0,2]], 3)).to.deep.equal([[-3,3],[-2,5],[0,2]])
-    })
-  })
-})
+ describe("test", () => {
+   it("true is true", () => {
+     expect(true).to.equal(true)
+   })
+ })
